5116f37a791be85264e3c90bdaa16a04d0205f37
Jeff Costa <cosjef@Mac-Desktop.local>
2012-12-16 15:17:37 -0500

    After Chapter 5

---
+++ chap5/guestbook/app/controllers/people_controller.rb
@@ -0,0 +1,92 @@
+class PeopleController < ApplicationController
+  # GET /people
+  # GET /people.json
+  # default page, show everything
+  def index
+    @people = Person.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @people }
+    end
+  end
+
+  # GET /people/1
+  # GET /people/1.json
+  # show only a specific user
+  def show
+    @person = Person.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @person }
+    end
+  end
+
+  # GET /people/new
+  # GET /people/new.json
+  def new
+    @person = Person.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @person }
+    end
+  end
+
+  # GET /people/1/edit
+  def edit
+    @person = Person.find(params[:id])
+  end
+
+  # POST /people
+  # POST /people.json
+  # POSTS a new record to the database
+  def create
+    @person = Person.new(params[:person])
+
+    respond_to do |format|
+      if @person.save
+        format.html { redirect_to @person, notice: 'Person was successfully created.' }
+        format.json { render json: @person, status: :created, location: @person }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @person.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /people/1
+  # PUT /people/1.json
+  # saves a record to the database
+  # PUT instead of POST
+  # finds user first, then saves the record to the database
+  # uses update_attributes method
+  def update
+    @person = Person.find(params[:id])
+
+    respond_to do |format|
+      if @person.update_attributes(params[:person])
+        format.html { redirect_to @person, notice: 'Person was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @person.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /people/1
+  # DELETE /people/1.json
+  # use the find method to locate the object to destroy by its :id
+  # then call the destroy method on it
+  def destroy
+    @person = Person.find(params[:id])
+    @person.destroy
+
+    respond_to do |format|
+      format.html { redirect_to people_url }
+      format.json { head :no_content }
+    end
+  end
+end
---
+++ chap5/guestbook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guestbook::Application.config.secret_token = '85a3f2b1ba2f1a79d84cb986930a8e1526ab7d34014cd9d763389444f62382df144353e9fef1d3002233aee3c21d9648965418908fe50230f2c6852e748c0d89'
