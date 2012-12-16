8b6969e89c63f3758207961a2301aa3765d88cdc
Jeff Costa <cosjef@Mac-Desktop.local>
2012-12-16 15:38:02 -0500

    Added some comments to Chap4

--- chap4/guestbook/app/controllers/entries_controller.rb
+++ chap4/guestbook/app/controllers/entries_controller.rb
@@ -2,6 +2,9 @@ class EntriesController < ApplicationController
 
 def sign_in
 # set the @name variable to the visitor_name request header sent back from the form	
+# visitor_name is the text field in the view
+# @name is the variable all the params are contained in
+
   	@name = params[:visitor_name]
  unless @name.blank?
 	@entry = Entry.create({:name => @name})
