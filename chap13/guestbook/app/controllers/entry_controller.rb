7e3090adc01d6be93dc2975272bd500b8b4d54d2
Jeff Costa <cosjef@gmail.com>
2012-12-31 15:17:23 -0500

    After finishing chapter 13

---
+++ chap13/guestbook/app/controllers/entry_controller.rb
@@ -0,0 +1,20 @@
+class EntryController < ApplicationController 
+def sign_in
+#get names array from session 
+@names=session[:names]
+#if the array doesn't exist, make one 
+unless @names
+@names=[] 
+end
+#get the latest name from the form 
+@name = params[:visitorName]
+if @name
+# add the new name to the names array 
+@names << @name
+end
+# store the new names array in the session
+session[:names]=@names 
+end
+end
+
+
---
+++ chap13/guestbook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guestbook::Application.config.secret_token = '4f0b162a306ea2e416da4f74fa3b8d7e58335e97af6b22f72dd21308a02af969cd87dd7d9728a25fbda04df0c3e25d0359e49ae674569464254bd60967f564c7'
